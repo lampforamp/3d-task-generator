bc34ab66af5114fe158cbde9185028fd05b72944
lampforamp <137897062+lampforamp@users.noreply.github.com>
2024-02-16 14:45:37 +0200

    Update objacts list

--- objects-list.js
+++ objects-list.js
@@ -1,3 +1,8 @@
 const objects = [
-    "Bluetooth speaker", "Wireless steamer", "Wireless Keyboard", "Kitchen Blender", "Kitchen knife", "Wireless Mouse"
+    "Portable speaker", "Handheld steamer", "Wireless Keyboard", "Kitchen Blender", "Cleaver", "Wireless Mouse", "Steam inhaler", "Paring knife", "Pocket knife", "Ultrasonic pest repeller", 
+    "Pocket radio", "Bike light", "Nikon binocular", "Baseus car charger", "Cordless flosser by Philips", "HDMI to mini HDMI adapter", "Knee pads", "Handheld milk frother", 
+    "Electric toothbrush by Oral-B", "Wooden hanger", "Adjustable walking cane", "Baby safety lock", "Fast charger by SBS", "Self grip hair rollers", "Fogless mirror", "Carbon card holder", 
+    "Leather dog collar", "Portable air compressor by Xiaomi", "Mosquito trap", "Pet nail clippers", "Pet nail grinder", "Outdoor bug zapper", "Smart scales", "Samsung smart watch", 
+    "Fitness tracker", "Digital thermometer", "Detangling brush", "Foil dispenser", "Alligator hair clips", "Measuring wheel", "3D Pen", "Office chair wheel", "Silicone spatula", 
+    "Bartender kit", "Wooden glasses", "Insoles by dr.Scholl's", "Nixie clock", "Portable electric iron", "Wine stopper", "Ceramic flat iron", "Calming collar", "3 in 1 charger"
 ]
