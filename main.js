ec1070fa6a4439054be10556070068db68b4700d
lampforamp <137897062+lampforamp@users.noreply.github.com>
2024-02-17 13:27:51 +0200

    jpg prefix fixed

--- main.js
+++ main.js
@@ -96,9 +96,9 @@ function generate () {
                 <h2>Here are the target style examples</h2>
                 <div class="images">
                     <img src="img/cartoon_1.jpg" alt="" onclick="openImage(this)"> 
-                    <img src="img/cartoon_2" alt="" onclick="openImage(this)">
-                    <img src="img/cartoon_3" alt="" onclick="openImage(this)">
-                    <img src="img/cartoon_4" alt="" onclick="openImage(this)">
+                    <img src="img/cartoon_2.jpg" alt="" onclick="openImage(this)">
+                    <img src="img/cartoon_3.jpg" alt="" onclick="openImage(this)">
+                    <img src="img/cartoon_4.jpg" alt="" onclick="openImage(this)">
                 </div>
               </div>
               `
